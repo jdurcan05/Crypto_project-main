0e4dc2028ea10a72b45e98594fcbb5eac30376bf
jdurcan05 <jdurcan05@gmail.com>
2025-11-17 07:49:05 -0500

    Added PEMS

---
+++ SiFTv1.0/server/gen_keys.py
@@ -0,0 +1,17 @@
+from Crypto.PublicKey import RSA
+
+#Idk if we need anything more
+
+pubkeyfile = "server_public_key.pem"
+privkeyfile = "server_private_key.pem"
+
+
+keypair = RSA.generate(2048)
+
+with open(pubkeyfile, 'wb') as f:
+        f.write(keypair.public_key().export_key(format='PEM'))
+        
+#excersize has a passkey. Would we want this? 
+with open(privkeyfile, 'wb') as f:
+        f.write(keypair.export_key(format='PEM'))
+
